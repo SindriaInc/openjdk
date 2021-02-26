d5e2d8ef2822fd488f8e3a214019b4b0a9e74895
lucapitzoi <luca.pitzoi@sindria.org>
2021-02-26 19:24:33 +0100

    Added java versions

--- build.sh
+++ build.sh
@@ -10,13 +10,23 @@ if [[ -z "$2" ]]; then
     exit 1
 fi
 
+if [[ -z "$3" ]]; then
+    echo "Provide a tag suffix as third argument (eg. 8)"
+    exit 1
+fi
+
 IMAGE_NAME=$1
 TAG_VERSION=$2
+TAG_SUFFIX=$3
 
+HOST_USER_UID=1000
 TIMEZONE=Europe/Rome
 
 docker build ./src \
-    --tag ${IMAGE_NAME}:${TAG_VERSION} \
+    --tag ${IMAGE_NAME}:${TAG_VERSION}-${TAG_SUFFIX} \
     --tag ${IMAGE_NAME}:latest \
     --build-arg TAG_VERSION=${TAG_VERSION} \
+    --build-arg TAG_SUFFIX=${TAG_SUFFIX} \
+    --build-arg HOST_USER_UID=${HOST_USER_UID} \
     --build-arg TIMEZONE=${TIMEZONE}
+
